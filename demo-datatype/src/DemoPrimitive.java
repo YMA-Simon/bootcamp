662e82a308cea2202aa22abfc3722091afa81514
srnot <yikmanau@gmail.com>
2024-08-06 11:08:56 +0800

    day2 morning1

---
+++ demo-datatype/src/DemoPrimitive.java
@@ -0,0 +1,15 @@
+public class DemoPrimitive {
+  public static void main(String[] args) {
+    int x = 100;
+    System.out.println(x);
+
+    int y;
+    y = -13;
+    System.out.println(y);
+    y = -100;
+    System.out.println(y);
+
+
+
+  }
+}
